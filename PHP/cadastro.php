6a689eef077d3d9b6126e97b550f957ae22484a7
Rafa <rasfaelmatos@gmail.com>
2023-11-15 23:13:05 -0300

    correcao de erro3

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
             if ($conn->query($sql) === TRUE) {
                 $aviso = "Cadastro realizado com sucesso!";
-                header("Location: Login.html");
+                header("Location: /git/TCC/Login.html");
             } else {
                 $aviso = "Erro no cadastro: " . $conn->error;
             }
@@ -53,38 +53,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>Cadastro</title>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="./images/icone.ico">
-    <link rel="stylesheet" type="text/css" href="./assets/style.css">
-    <link rel="stylesheet" href="./assets/login.css">
-    
-    <!-- Adicione o código JavaScript para exibir o pop-up -->
-    <script>
-    function exibirPopup(aviso) {
-        alert(aviso);
-    }
-    </script>
+    <link rel="shortcut icon" type="imagex/png" href="../images/icone.ico">
+    <link rel="stylesheet" href="../Css/form.css">
 </head>
 <body>
 <div class="container">
 
-    <div> <img src="./images/cadastro.png" alt="Cadastro"></div>
+    <div> <img src="../images/cadastro.png"></div>
     <!--FORMULÁRIO DE CADASTRO-->
-    <div id="cadastro">
-    <?php
-    if (!empty($aviso)) {
-        // Chame a função de pop-up JavaScript
-        echo '<script>exibirPopup("' . $aviso . '");</script>';
-    }
-    ?>
-    <form method="post" action="cadastro.php">
+    <div id="login">
+    <form method="post">
         <h1 style="text-align: center">Cadastrar</h1> <br>
 
         <label for="nome">Nome:</label>
         <input id="nome" name="nome" required="required" type="text" placeholder="Nome Sobrenome"/><br>
 
-        <label for "email">E-mail:</label>
+        <label for="email">E-mail:</label>
         <input id="email" name="email" required="required" type="text" placeholder="contato@gmail.com"/>
 
         <label for="senha">Senha:</label>
@@ -93,10 +79,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div style="text-align: center">
             <input type="submit" value="Cadastrar" class="button"/><a href="index.html"></a>
             <br><br><br>
-            <h4 style="text-align: right;">Ainda não tem conta? <a href="Login.html">Logar</a></h4>
+            <h4 style="text-align: right;">Já tem uma conta? <a href="Login.html">Logar</a></h4>
         </div>
     </form>
     </div>
 </div>
+
+<script>
+    function exibirPopup(aviso) {
+        alert(aviso);
+    }
+</script>
 </body>
 </html>
