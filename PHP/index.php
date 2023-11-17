a19187d7e125237b82e18524b45f9074bd2b9b1b
Hilary Brandão <118312974+Gadiinha@users.noreply.github.com>
2023-11-17 17:37:08 -0300

    Adicionei o fórum
    cabeçalho nn tem ainda

--- PHP/index.php
+++ PHP/index.php
@@ -1,30 +1,42 @@
-<!--HOW TO DEVELOP DISCUSSION FORUM OR COMMENT MANAGEMENT SYSTEM USING PHP MYSQL
-https://youtu.be/TRYdsaxUvzA-->
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="icon" href="./images/favicon.png" type="image/png" sizes="16x16">
-    <title>Fórum</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-      <script src="./JS/main.js"></script>
-  </head>
+<head>
+<link rel="icon" href="./images/favicon.png" type="image/png" sizes="16x16">
+<title>forum</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="./Css/calculadora.css">
+  <link rel="stylesheet" href="./Css/style.css">
+  <script src="forum.js"></script>
+  <style>
+    .container {
+            text-align: center;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
 
-  <div class="container">
-        <div class="subforum">
-            <h2 style="text-align: center">Bem vindo(a) a seção Fórum!</h2>
-            <img src="./images/duvida.svg" style="display: block; margin: 0 auto; height: 400px; border-radius: 5px;">
-                <div >
-                    <p>Neste espaço, você encontrará um ambiente dedicado a discutir
-                        e explorar o fascinante mundo da Matemática.
-                        Este é o lugar perfeito para compartilhar conhecimentos,
-                        fazer perguntas e aprender juntos.</p><br><br>
+        p {
+            font-size: 18px;
+            line-height: 1.6;
+            margin-top: 20px;
+        }
+  </style>
+</head>
+
+<body>
+    <div class="container">
+            <h2>Bem vindo(a) ao Fórum!</h2>
+            <img src="./images/cefetmat.png" style="display: block; margin: 0 auto; height: 400px; border-radius: 5px;">
+                <div>
+                    <p>Neste espaço, você encontrará um ambiente dedicado a discutir e explorar o fascinante mundo da Matemática.
+                        Este é o lugar perfeito para compartilhar conhecimentos, fazer perguntas e aprender juntos.</p><br><br>  
                 </div>
-        </div>
-<!--  Página de resposta-->
+    </div>
+<!-- Modal -->
 <div id="ReplyModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
@@ -32,7 +44,7 @@ https://youtu.be/TRYdsaxUvzA-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Responder</h4>
+        <h4 class="modal-title">Responda a dúvida</h4>
       </div>
       <div class="modal-body">
         <form name="frm1" method="post">
@@ -42,34 +54,33 @@ https://youtu.be/TRYdsaxUvzA-->
         	  <input type="text" class="form-control" name="Rname" required>
         	</div>
             <div class="form-group">
-              <label for="comment">Justificativa:</label>
+              <label for="comment">Explicação:</label>
               <textarea class="form-control" rows="5" name="Rmsg" required></textarea>
             </div>
-        	 <input type="button" id="btnreply" name="btnreply" class="btn btn-primary" value="Responder">
+        	 <input type="button" id="btnreply" name="btnreply" class="button" value="Responder">
       </form>
       </div>
     </div>
-
   </div>
 </div>
 
-
 <div class="container">
+
 <div class="panel panel-default" style="margin-top:50px">
   <div class="panel-body">
-    <h3>FORUM</h3>
+    <h3>Envie sua dúvida!</h3>
     <hr>
     <form name="frm" method="post">
         <input type="hidden" id="commentid" name="Pcommentid" value="0">
 	<div class="form-group">
-	  <label for="usr">NOME:</label>
+	  <label for="usr">Título da pergunta:</label>
 	  <input type="text" class="form-control" name="name" required>
 	</div>
     <div class="form-group">
-      <label for="comment">QUESTÃO:</label>
+      <label for="comment">Descrição:</label>
       <textarea class="form-control" rows="5" name="msg" required></textarea>
     </div>
-	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
+	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Enviar">
   </form>
   </div>
 </div>
@@ -77,7 +88,7 @@ https://youtu.be/TRYdsaxUvzA-->
 
 <div class="panel panel-default">
   <div class="panel-body">
-    <h4>Questões recentes</h4>           
+    <h4>Perguntas recentes</h4>           
 	<table class="table" id="MyTable" style="background-color: #edfafa; border:0px;border-radius:10px">
 	  <tbody id="record">
 	  </tbody>
